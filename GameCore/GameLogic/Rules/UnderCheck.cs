a3077669fdddfb79056fab40e9f1a1601e3a5771
SergioStankovik <moonburn.co@gmail.com>
2021-05-26 12:26:31 +0200

    Adde CheckAfterMove
    Method checks if player will be under check if move is made

--- GameCore/GameLogic/Rules/UnderCheck.cs
+++ GameCore/GameLogic/Rules/UnderCheck.cs
@@ -27,6 +27,33 @@ namespace GameCore
             return false;
         }
 
+        internal static bool CheckAfterMove(Move move) 
+        {
+
+            // Save pieces
+            Piece startPiece = new(GameState.Board[move.start].color, GameState.Board[move.start].type);
+        
+            Piece endPiece = new Piece();  
+            bool endHavePiece = GameState.Board.ContainsKey(move.end);
+            if (endHavePiece) endPiece = new(GameState.Board[move.end].color, GameState.Board[move.end].type);
+
+            // Make move
+            if (endHavePiece) GameState.Board.Remove(move.end);
+            GameState.Board.Remove(move.start);
+            GameState.Board.Add(move.end, startPiece);
+
+            // Check 
+            bool underCheck = startPiece.color == Color.White ? White() : Black();
+
+            // Back to normal
+            GameState.Board.Remove(move.end);
+            GameState.Board.Add(move.start, startPiece);
+            
+            if(endHavePiece) GameState.Board.Add(move.end, endPiece);
+
+            return underCheck;
+        }
+
         private static bool PieceAttackingSquare(Square origin, Square target) 
         {
             switch (GameState.Board[origin].type)
